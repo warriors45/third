6e8b0bf169450265baac3b930d9d9f4753d04744
warriors45 <41731136+warriors45@users.noreply.github.com>
2018-07-27 11:42:59 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+ 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Alphabet 
+{
+ 
+    public static void main(String[] args) 
+    {
+ 
+        char c;
+ 
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println( " Alphabet");
+        else
+            System.out.println("No");
+    }
+}
