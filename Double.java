c83516585a28d90a707c205e3f06a353bb429f58
warriors45 <41731136+warriors45@users.noreply.github.com>
2018-07-27 12:20:17 +0530

    Create Double.java

---
+++ Double.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Double
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner i=new Scanner(System.in);
+		int a=i.nextInt();
+		for(int j=1;j<=a;j++)
+		{
+			System.out.println("Hello");
+		}
+	}
+}
