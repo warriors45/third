b4a4366f8ba6b255e9d4de79a4ab4d06ff47bc4b
warriors45 <41731136+warriors45@users.noreply.github.com>
2018-07-30 16:37:18 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class even
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+    Scanner input=new Scanner(System.in);
+    int n=input.nextInt();
+    int m=input.nextInt();
+    for (int i=n+1;i<m;i++)
+    {
+    	if(i%2==0)
+    	{
+    		System.out.print(i+" ");
+    	}
+    }
+	}
+}
