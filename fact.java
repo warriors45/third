cc5d20bd0d74e6b596901097c7f4b400b133a8d1
warriors45 <41731136+warriors45@users.noreply.github.com>
2018-07-31 11:10:08 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class fact
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner input=new Scanner(System.in);
+		int n=input.nextInt();
+		int fact=1;
+		int i;
+		for(i=2;i<=n;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact+" ");
+	}
+}
