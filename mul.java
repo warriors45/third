5c8a7c914c8d10cfc170e98564f59fb062971b01
warriors45 <41731136+warriors45@users.noreply.github.com>
2018-08-08 10:49:52 +0530

    Create mul.java

---
+++ mul.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class mul
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner input=new Scanner(System.in);
+	int n=input.nextInt();
+	int i;
+	int mul;
+for(i=1;i<=5;i++)
+	{
+		mul=n*i;
+		System.out.print(mul+" ");
+	}
+	}
+}
