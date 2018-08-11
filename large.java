c537530eee00d936667b700417a2b37fbad78652
warriors45 <41731136+warriors45@users.noreply.github.com>
2018-08-11 12:49:15 +0530

    Create large.java

---
+++ large.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner input=new Scanner(System.in);
+		int n=input.nextInt();
+		int a[]=new int[n];
+		int i;
+		for(i=0;i<n;i++)
+		{
+		 a[i]=input.nextInt();
+		}
+		int large=a[0];
+		for(i=0;i<n;i++)
+		{
+			if(large<a[i])
+			{
+			large=a[i];
+			}
+		}
+			System.out.println(large);
+	}
+}
