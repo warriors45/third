c84d60b461aa45fed073f98934192eb386aadbc1
warriors45 <41731136+warriors45@users.noreply.github.com>
2018-08-13 12:52:17 +0530

    Create median.java

---
+++ median.java
@@ -0,0 +1,34 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner input=new Scanner(System.in);
+		int n=input.nextInt();
+		int a[]=new int[n];
+		int i,j,temp;
+		for(i=0;i<n;i++)
+		{
+			a[i]=input.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+			for(j=i;j<n;j++)
+			{
+			if(a[i]>a[j])
+			{
+				temp=a[j];
+				a[j]=a[i];
+				a[i]=temp;
+			}
+			}
+		}
+		System.out.print(a[n/2]+" ");
+	}
+}
