34d5564d0a0238806e74bdc2ec7d4594ef818e03
warriors45 <41731136+warriors45@users.noreply.github.com>
2018-08-14 12:24:13 +0530

    Create count2.java

---
+++ count2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int count=0;
+		for(char c:s.toCharArray())
+		{
+			if(c!=' ')
+			count++;
+		}
+		System.out.println(count);
+	}
+}
+© 2018 GitHub, Inc.
