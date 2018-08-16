47e6c4edad6627877bf3789506efcb1e228fbe9d
warriors45 <41731136+warriors45@users.noreply.github.com>
2018-08-16 11:20:48 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner input=new Scanner(System.in);
+		String s=input.nextLine();
+		int n=input.nextInt();
+		int i;
+		for(i=0;i<n;i++)
+		{
+			System.out.println(s);
+		}
+	}
+}
