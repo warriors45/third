cd9cf59e66af90523c2a9ef279ad07a026af666a
warriors45 <41731136+warriors45@users.noreply.github.com>
2018-09-08 20:50:46 +0530

    Create 52.java

---
+++ 52.java
@@ -0,0 +1,48 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner input=new Scanner(System.in);
+		int n=input.nextInt();
+		switch(n)
+		{
+			case 1: 
+				System.out.println("One");
+				break;
+				case 2: 
+				System.out.println("Two");
+				break;
+				case 3: 
+				System.out.println("Three");
+				break;
+				case 4: 
+				System.out.println("Four");
+				break;
+				case 5: 
+				System.out.println("Five");
+				break;
+				case 6: 
+				System.out.println("Six");
+				break;
+				case 7: 
+				System.out.println("Seven");
+				break;
+				case 8: 
+				System.out.println("Eight");
+				break;
+				case 9: 
+				System.out.println("Nine");
+				break;
+				case 10: 
+				System.out.println("Ten");
+				break;
+		}
+	}
+}
