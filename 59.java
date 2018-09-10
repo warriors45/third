3955b5ba6e8b0260652f04ddd393ab787f8e3c47
warriors45 <41731136+warriors45@users.noreply.github.com>
2018-09-10 21:08:34 +0530

    Rename 58.java to 59.java

---
+++ 59.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+          Scanner input=new Scanner(System.in);
+          int n=input.nextInt();
+          int count=0;
+          while(n!=0)
+         {
+          n=n/10;
+          count++;
+         }
+         System.out.println(count);
+	}
+}
